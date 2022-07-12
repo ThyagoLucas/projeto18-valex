fcc964fa3876e3467bef1a6f10346e9f11e0142b
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-07-12 13:25:29 -0400

    make is better heroku

--- database.ts
+++ database.ts
@@ -5,17 +5,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
+
 export const connection = new Pool({
-
-  user:'postgres',
-  password:'postgres',
-  host: 'localhost',
-  port:5432,
-  database:'valex'
-
-})
-
-
-//export const connection = new Pool({
-  //connectionString: process.env.DATABASE_URL,
-//});
+  connectionString: process.env.DATABASE_URL,
+});
