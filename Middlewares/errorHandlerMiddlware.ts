fb58daf72089dde3ba00ce7cf445b7ea0552f42c
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-07-12 15:23:56 -0400

    make is better heroku

--- Middlewares/errorHandlerMiddlware.ts
+++ Middlewares/errorHandlerMiddlware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function errorHandler (error, req: Request, res: Response, next: NextFunction){
     
-    console.log('errosood:', error);
+    console.log('error:', error);
     if(error) {
         return res.status(404).send(error.message);
     }
